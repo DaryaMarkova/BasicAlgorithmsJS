ee54e52fce57d0c540890759ad9d6f1080f4edd3
darya-markova <darya.towarddreams@gmail.com>
2020-03-21 14:23:28 +0300

    searching of tree max height problem

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { BraceValidation } from "./brace.validation";
+import { TreeHeight } from "../tree.height";
 
-const validator = new BraceValidation();
-validator.run("{{{**[][][]");
+// const validator = new BraceValidation();
+// validator.run("{{{**[][][]");
+
+const tree = new TreeHeight([2,2,3,5,5,7,7,9,9,-1]);
+console.log(tree.maxHeight);
---
+++ tree.height.js
@@ -0,0 +1,57 @@
+/*
+* given a tree that is presented with two params
+* [4, -1, 4, 1, 1] - list of nodes where <nodes[i]> is parent of <i>, 
+*  if nodes[i] == -1, then <i> is root 
+*/
+
+export class TreeHeight {
+	constructor(list) {
+		this.tree = {}; 
+		this.cache = {};
+		this.count = list.length;
+		
+		this.initTree(list);
+	}
+
+	initTree(list) {
+		for (let i = 0; i < list.length; i++) {
+			this.tree[i] = list[i]
+		}
+	}
+
+	// the length of way from node to the root
+	traverse(node) {
+		let length = 1;
+
+    while ((node = this.tree[node]) >= 0) {	
+			if (this.cache[node]) {
+				length += this.cache[node];
+				break;
+			} else {
+				++length;
+			}
+		}
+
+		return length;
+	}
+
+	// traversing from all the nodes and searching maxHeight
+	get maxHeight() {
+		let maxHeight = 0, height;
+
+		for (let i = 0; i < this.count; i++) {
+			if (this.cache[i]) {
+				height = this.cache[i];
+			} else {
+				height = this.traverse(i);
+				
+				this.cache[i] = height;
+				this.cache[this.tree[i]] = height - 1;
+			}
+
+			maxHeight = Math.max(maxHeight, height);
+		}
+
+		return maxHeight;
+	}
+}
